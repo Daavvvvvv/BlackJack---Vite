fc1804c99827c0724782b87e566433f925065c37
Daavvvvvv <davidvelez006@gmail.com>
2024-03-19 17:47:11 -0500

    modularizacion con vite, blackjack

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+export {createDeck} from './createDeck'
+export {pedirCarta} from './askCard'
+export {valorCarta} from './cardValue'
+export {turnoComputadora} from './Turnos'
+export {crearCartaHTML} from './crear-carta-html'
+export {} from './buttons'
